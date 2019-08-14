6382b27fef716e1d3ed8e247952de3725c7a8d33
github username <github email address>
2019-08-14 10:26:49 -0500

    JLH

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,26 @@
 class Customer
   attr_reader :first_name, :last_name
+  attr_accessor :customer
+
+  @@all = []
+  @@reviews = []
+  @@restaurants = []
+
+  def self.all
+    return @@all
+  end
+
+  def self.find_by_name(name)
+
+  end
+
+  def self.find_all_by_first_name(name)
+
+  end
+  
+  def self.all_names
+
+  end
 
   def initialize(first_name, last_name)
     @first_name = first_name
@@ -9,4 +30,25 @@ class Customer
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def add_review(restaurant, content, rating)
+    newReview = Review.new(restaurant, content, rating)
+    newReview.customer = self
+    @@restaurants << restaurant
+  end
+
+  def num_reviews
+    numReviews = []
+    Review.all.each do |review|
+      if review.customer = self
+        numReviews << review
+      end
+    end
+    return numReviews.length
+  end
+
+  def restaurants
+    return @@restaurants
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,44 @@
 class Restaurant
+
+  attr_accessor :restaurant, :customers
   attr_reader :name
+
+  @@all = []
+
+  def self.all
+    return @@all
+  end
+
+  def self.find_by_name(name)
+
+  end
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def customers
+    customers = []
+    Review.all.each do |review|
+      if review.restaurant = self
+        customers << review.customer
+        # puts review.customer
+      end
+    end
+    return customers
+  end
+
+  def reviews
+
+  end
+
+  def average_star_rating
+
+  end
+
+  def longest_review
+
   end
 
 end
