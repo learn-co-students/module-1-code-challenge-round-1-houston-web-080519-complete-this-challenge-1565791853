9059431db353ae816588a52e78e324c17cdf8350
BarretteBanner <barrettebanner@gmail.com>
2019-08-14 10:23:30 -0500

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,75 @@
 class Restaurant
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def customers
+    array1 = []
+    Restaurant.all.each do |restaurant|
+      Review.all.each do |review|
+        if review.restaurant == restaurant
+          array1 << review.customer
+        end
+      end
+    end
+    array1.uniq
+  end
+
+  def reviews
+    array1 = []
+    Review.all.each do |review|
+      if review.restaurant == self
+        array1 << review
+      end
+    end
+    array1
+  end
+    
+
+
+  def average_star_rating
+    array1 = []
+    Review.all.each do |review|
+      if review.restaurant == self
+        array1 << review.rating
+      end
+    end
+    length = array1.length 
+    sum = array1.sum
+    puts sum/length
+  end
+
+  def longest_review
+    most = 0
+    name = nil
+    Review.all.each do |review|
+      stuff = []
+      if review.restaurant == self
+        stuff << review.content
+      end
+      if stuff.length > most 
+        most = stuff.length
+        name = review
+      end
+    end
+      name
+  end
+
+  def self.find_by_name(name)
+    array = []
+    Restaurant.all.each do |place|
+      if place == name
+        array << place
+      end
+    end
+    array
+  end
 end
+
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,43 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-binding.pry
+# binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
+
+
+
+John = Customer.new("John", "Ball")
+Mike = Customer.new("Mike", "Donland")
+Barrette = Customer.new("Barrette", "Banner")
+customer4 = Customer.new("Barrette", "Jones")
+Subway = Restaurant.new("Subway")
+Burger = Restaurant.new("Burger")
+John.add_review(Subway, "The food is so tasty!", 5)
+Mike.add_review(Burger, "The food is so bad!", 1)
+Barrette.add_review(Subway, "The service is great!", 5)
+John.add_review(Burger, "The food is not very good!", 2)
+Barrette.add_review(Burger, "The service is okay-ish", 2)
+Barrette.add_review(Subway, "The food is still amazing", 5)
+John.add_review(Subway, "Tasty...Tasty...Tasty!", 5)
+
+
+
+
+
+
+
+
+#puts Customer.all
+#puts Restaurant.all
+#puts Review.all
+#puts John.num_reviews
+#puts Barrette.num_reviews
+#puts Barrette.restaurant
+#puts Subway.customers
+#puts Subway.reviews
+#puts Subway.average_star_rating
+#puts Subway.longest_review.content
+#puts Customer.find_by_name(Barrette)
+#puts Customer.find_all_by_first_name("Barrette")
+#puts Customer.all_names
+puts Restaurant.find_by_name(Subway)
