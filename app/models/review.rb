3cd0835b2cda2a074e5ad2e2481646416479a3bd
Richard Pitts <Richardepitts@gmail.com>
2019-08-14 10:25:52 -0500

    -

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,51 @@
 class Review
-  
+
+      ## My Code
+
+      @@all = []
+
+      attr_accessor :restaurant, :content, :rating, :customer
+
+    # given a restaurant object, some review content 
+    # (as a string), and a star rating (as an integer), 
+    # creates a new review and associates it with that 
+    # customer and restaurant.
+      def initialize(restaurant, content, rating)
+        @restaurant = restaurant
+        @content = content
+        @rating = rating
+        @@all << self
+      end
+
+      def self.all
+        @@all
+      end
+
+      def customer
+        newArray = []
+        Customer.all.select do | customer |
+            if customer.review == self
+                newArray.push(customer)
+            end
+        end
+        return newArray
+      end
+
+
+      def restaurant
+        Restaurant.all.select do | restaurant |
+            if restaurant.review == self
+                newArray.push(restaurant) 
+            end
+        end
+        return newArray
+      end 
+
+      def content 
+        @content 
+      end 
+
+
+
 end
 
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,21 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-binding.pry
+# binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
+
+customer1 = Customer.new("Richard", "Pitts", "Tony's")
+customer2 = Customer.new("John", "Smith", "Flemings")
+customer3 = Customer.new("Richard", "Jones", "Chick-fil-a")
+customer4 = Customer.new("Richard2", "Jones2", "Tony's")
+
+# name1 = "Richard"
+# p Customer.find_all_by_first_name(name1)
+
+restaurant1 = Restaurant.new("Tony", "a", "b", "c")
+restaurant2 = Restaurant.new("Tony2", "a", "b", "c")
+restaurant3 = Restaurant.new("Tony3", "a", "b", "c")
+# p restaurant1.customers
+
+name2 = "Tony"
+p Restaurant.find_by_name(name2)
