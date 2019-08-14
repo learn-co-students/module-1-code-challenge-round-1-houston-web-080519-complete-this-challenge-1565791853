34e326c0992a43af9abd6a2472bb2cd5d7c499e4
Emily <mle.jo.11@gmail.com>
2019-08-14 10:27:04 -0500

    mod 1 coding challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer
+  @@all = []
   attr_reader :first_name, :last_name
 
   def initialize(first_name, last_name)
@@ -6,7 +7,34 @@ class Customer
     @last_name  = last_name
   end
 
+  def self.all
+    @@all
+  end
+
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def add_review(restaurant, content, rating)
+    
+  end
+
+  def find_by_name(name)
+    
+    Customer.full_name.find |
+  end
 end
+
+
+# - `Customer.find_by_name(name)`
+#   - given a string of a **full name**, returns the **first customer** whose full name matches
+# - `Customer.find_all_by_first_name(name)`
+#   - given a string of a first name, returns an **array** containing all customers with that first name
+# - `Customer.all_names`
+#   - should return an **array** of all of the customer full names
+# - `Customer#add_review(restaurant, content, rating)`
+#   - given a **restaurant object**, some review content (as a string), and a star rating (as an integer), creates a new review and associates it with that customer and restaurant.
+# - `Customer#num_reviews`
+#   - Returns the total number of reviews that a customer has authored
+# - `Customer#restaurants`
+#   - Returns a **unique** array of all restaurants a customer has reviewed
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,32 @@
 class Review
+    @@all = []
+    attr_accessor :customer, :restaurant, :content, :rating
+
+    def initialize(customer, restaurant, content, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @content = content
+        @rating = rating
+        @@all.push(self)
+        
+    end
+
+    def self.all
+        @@all
+    end
   
 end
-
+# - `Restaurant#customers`
+#   - Returns a **unique** list of all customers who have reviewed a particular restaurant.
+# - `Restaurant#reviews`
+#   - returns an array of all reviews for that restaurant
+# - `Restaurant#average_star_rating`
+#   - returns the average star rating for a restaurant based on its reviews
+# - `Restaurant#longest_review`
+#   - returns the longest review content for a given restaurant
+# - `Customer#add_review(restaurant, content, rating)`
+#   - given a **restaurant object**, some review content (as a string), and a star rating (as an integer), creates a new review and associates it with that customer and restaurant.
+# - `Customer#num_reviews`
+#   - Returns the total number of reviews that a customer has authored
+# - `Customer#restaurants`
+#   - Returns a **unique** array of all restaurants a customer has reviewed
