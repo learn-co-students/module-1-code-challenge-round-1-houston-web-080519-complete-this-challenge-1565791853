08e14b7894d65eaf147275f5331316af057be5af
Your Name <you@example.com>
2019-08-14 10:26:03 -0500

    -

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,37 @@
 class Customer
-  attr_reader :first_name, :last_name
-
-  def initialize(first_name, last_name)
+  attr_reader :first_name, :last_name, :restaurant
+  @@all = []
+  def initialize(first_name, last_name, restaurant)
     @first_name = first_name
     @last_name  = last_name
+    @restaurant = restaurant
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(full_name)
+    first_customer = find do {|full_name| Customer.full_name}
+      return first_customer
+    end
+
+  def self.find_by_first_name(first_name)
+    array = []
+    array.all.push(first_name)
+  end
+
+  def self.all_names
+    array = []
+    array.each.push(full_name)
+  end
+
+  def num_reviews 
+    
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review
-  
+  @@all = []
+  attr_accessor :restaurant, :rating
+  def initialize 
+    @restaurant = restaurant
+    @rating = rating 
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customer 
+    
 end
 
